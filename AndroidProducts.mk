606f4b5e49c35217a78f84e431d87e9023e9001f
Oliwier Nogaj <56972326+WindowsDev2021@users.noreply.github.com>
2023-01-19 20:46:24 +0100

    starlte: TequilaOS bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_starlte.mk
+    $(LOCAL_DIR)/tequila_starlte.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_starlte-eng \
-    aosp_starlte-user \
-    aosp_starlte-userdebug
+    tequila_starlte-eng \
+    tequila_starlte-user \
+    tequila_starlte-userdebug
--- tequila_starlte.mk
+++ tequila_starlte.mk
@@ -29,11 +29,11 @@ TARGET_SCREEN_HEIGHT := 2960
 TARGET_SCREEN_WIDTH := 1440
 TARGET_BOOT_ANIMATION_RES := 1440
 
-## Inherit some common AOSP stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+## Inherit some common TequilaOS stuff
+$(call inherit-product, vendor/tequila/config/common_full_phone.mk)
 
 ## Device identifier, this must come after all inclusions
-PRODUCT_NAME := aosp_starlte
+PRODUCT_NAME := tequila_starlte
 PRODUCT_DEVICE := starlte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G960F
